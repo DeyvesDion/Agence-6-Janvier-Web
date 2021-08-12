22fea59319a4fa03e8bf27641eabb61d093ab63d
DeyvesDion <deyvesdion@gmail.com>
2021-08-12 19:18:45 +0200

    first commit

---
+++ components/Hero.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="bg-gradient-to-r from-yellow-200 via-red-300 to-pink-300 container h-screen inline-flex content-between justify-between items-center"
+  >
+    <div class="w-4/5 px-6 py-20">
+      <div class="items-center lg:flex">
+        <div class="w-full lg:w-1/2">
+          <div class="lg:max-w-lg">
+            <h1
+              class="text-2xl font-semibold text-gray-800 uppercase dark:text-white lg:text-3xl"
+            >
+              Best Place To Choose Your Clothes
+            </h1>
+            <p class="mt-2 text-gray-600 dark:text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro
+              beatae error laborum ab amet sunt recusandae? Reiciendis natus
+              perspiciatis optio.
+            </p>
+            <button
+              class="w-full px-3 py-2 mt-6 text-xs font-medium text-white uppercase transition-colors duration-200 transform bg-indigo-600 rounded-md lg:w-auto hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500"
+            >
+              Shop Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+      <img class="w-full h-full lg:max-w-2xl" src="" alt="Catalogue-pana.svg" />
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NavBar />
+    <Hero />
+  </div>
+</template>
+
+<style scoped>
+* {
+  margin: Opx;
+  padding: Opx;
+  box-sizing: border-box;
+}
+</style>
